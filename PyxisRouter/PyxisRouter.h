0ae2af33778d8ee9818f2bbd69c024188cb9abc4
Seth Faxon <seth.faxon@gmail.com>
2015-07-21 14:38:18 -0700

    Initial Commit

---
+++ PyxisRouter/PyxisRouter.h
@@ -0,0 +1,19 @@
+//
+//  PyxisRouter.h
+//  PyxisRouter
+//
+//  Created by Seth Faxon on 7/21/15.
+//  Copyright © 2015 SlashAndBurn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PyxisRouter.
+FOUNDATION_EXPORT double PyxisRouterVersionNumber;
+
+//! Project version string for PyxisRouter.
+FOUNDATION_EXPORT const unsigned char PyxisRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PyxisRouter/PublicHeader.h>
+
+
